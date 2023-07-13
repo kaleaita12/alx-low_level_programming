e58ca644b5714305939d11ae9d6a2bab09875984
Your Name <you@example.com>
2023-07-13 14:38:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
