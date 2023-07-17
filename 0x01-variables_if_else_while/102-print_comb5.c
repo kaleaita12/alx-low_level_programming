c6addf914e22d0ff864f438495aa51945042155e
Your Name <you@example.com>
2023-07-17 12:07:41 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,20 +14,20 @@ int main(void)
 
         for (p = 0; p <= 98; p++)
         {
-
-        for (q = p + 1; q <= 99; q++)
-        {
-        putchar((p / 10) + '0');
-        putchar((p % 10) + '0');
-        putchar(' ');
-        putchar((q / 10) + '0');
-        putchar((q % 10) + '0');
-        if (p == 98 && p == 99)
-        continue;
-        putchar(',');
-        putchar(' ');
-        }
+		for (q = p + 1; q <= 99; q++)
+       		 {
+       			 putchar((p / 10) + '0');
+      		 	 putchar((p % 10) + '0');
+        		 putchar(' ');
+        		 putchar((q / 10) + '0');
+       			 putchar((q % 10) + '0');
+       			 if (p == 98 && p == 99)
+       				 continue;
+       			 putchar(',');
+       			 putchar(' ');
+       		 }
         }
         putchar('\n');
-        return (0);
+       
+       	return (0);
 }
