83d198fa97051071440fbe91a208f25825a6f04a
Your Name <you@example.com>
2023-07-17 08:57:30 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ *
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+        int i = 'a';
+        int j = 'A';
+        while ( i <= 'z')
+        {
+                putchar (i);
+                i++ ;
+        }
+        while ( j<= 'Z')
+        {
+                putchar (j);
+                j++ ;
+        }
+        putchar ('\n');
+        return (0);
+}
+
